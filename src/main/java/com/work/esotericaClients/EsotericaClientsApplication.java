ebd0cf4a0a00b72bf2a5dcd3095d862b0815bfcc
h1ud <its.hludw1g@gmail.com>
2026-05-26 22:24:13 -0500

    Initial commit

---
+++ src/main/java/com/work/esotericaClients/EsotericaClientsApplication.java
@@ -0,0 +1,13 @@
+package com.work.esotericaClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsotericaClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsotericaClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/work/esotericaClients/EsotericaClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.work.esotericaClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsotericaClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
